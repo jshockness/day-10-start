753a3a2691175f28c0e7582db0bd79aac25fe94f
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-18 14:44:22 +0000

    step 2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Functions Outputs
 
 def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
   first = f_name.title()
   last = l_name.title()
   return f"{first} {last}"
 
-print(format_name("JASon", "SHockNess"))
+print(format_name(input("What is your first name? :"), input("What is your last name? :")))
