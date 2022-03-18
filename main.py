45301cbb9e86b63961bfc779f6a0fa9d4bc53295
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-18 15:12:57 +0000

    docstrings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Functions Outputs
 
 def format_name(f_name, l_name):
+  """Takes first and last name and format it to return the title case verison of the name."""
   if f_name == "" or l_name == "":
     return "You didn't provide valid inputs."
   first = f_name.title()
@@ -8,3 +9,5 @@ def format_name(f_name, l_name):
   return f"{first} {last}"
 
 print(format_name(input("What is your first name? :"), input("What is your last name? :")))
+
+form
